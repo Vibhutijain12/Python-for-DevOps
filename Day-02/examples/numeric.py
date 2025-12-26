1c3039a5e70960c6a044e834cada3d0dc4ba6859
Vibhuti Jain <vibhutijain9812@gmail.com>
2025-12-26 23:08:07 +0530

    numeric.py

---
+++ Day-02/examples/numeric.py
@@ -0,0 +1,28 @@
+a = 12   # Integer
+b = 5.25 # Float
+
+print(a + b)     # Print the sum of a and b
+print(int(a + b))  # Convert the sum to an integer and print it
+
+
+## To verify the type of any object in Python, use the type() function:
+
+print(type(a))
+print(type(b))
+
+## Insert the correct syntax to convert x into a floating point number.
+x = 5
+x = float(x)
+print(x)   ## output: 5.0
+
+## Insert the correct syntax to convert x into a integer.
+
+x = 5.55
+x = int(x) 
+print(x)   ## output: 5
+
+## Insert the correct syntax to convert x into a complex number.
+
+x = 5 
+x = complex(x)
+print(x)   ## output: (5+0j)
